27f9a7880b36518f5187edbc0a8c9a1b3f6c5828
coutinho21 <guilhermecscoutinho@gmail.com>
2023-10-27 12:04:43 +0100

    Fix board

--- user_interface.pl
+++ user_interface.pl
@@ -1,16 +1,18 @@
 % Prints the board
 print_board :-
-    print_rows(1, 9).
+    print_rows(1, 10).
 
 % Prints the rows of the board
+print_rows(X,X).
 print_rows(Row, RowEnd) :-
     Row =< RowEnd,
     nl,
-    print_row(Row, 1, 7),
+    print_row(Row, 1, 10),
     NextRow is Row + 1,
     print_rows(NextRow, RowEnd).
 
 % Prints a row of the board
+print_row(_,X,X).
 print_row(Row, Col, ColEnd) :-
     Col =< ColEnd,
     cell(Row, Col, Symbol),
@@ -19,5 +21,5 @@ print_row(Row, Col, ColEnd) :-
     print_row(Row, NextCol, ColEnd).
 
 % Define the way a cell is printed
-write_cell(Symbol) :- write('|'), write(Symbol). % Symbol
+write_cell(Symbol) :- write(Symbol). % Symbol
 
