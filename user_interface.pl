637f7ef030c541ed2e22f0733b79668693ec3b1f
xouteiro <xavierouteiro@gmail.com>
2023-10-29 15:26:20 +0000

    finished initial board with 4 teams

--- user_interface.pl
+++ user_interface.pl
@@ -1,18 +1,18 @@
 % Prints the board
 print_board :-
-    print_rows(1, 10).
+    print_rows(1, 14).
 
 % Prints the rows of the board
 print_rows(X,X).
 print_rows(Row, RowEnd) :-
     Row =< RowEnd,
     nl,
-    print_row(Row, 1, 10),
+    print_row(Row, 1, 14),
     NextRow is Row + 1,
     print_rows(NextRow, RowEnd).
 
 % Prints a row of the board
-print_row(_,X,X).
+print_row(_, ColEnd, ColEnd) :- write('|'), nl.
 print_row(Row, Col, ColEnd) :-
     Col =< ColEnd,
     cell(Row, Col, Symbol),
@@ -21,5 +21,5 @@ print_row(Row, Col, ColEnd) :-
     print_row(Row, NextCol, ColEnd).
 
 % Define the way a cell is printed
-write_cell(Symbol) :- write(Symbol). % Symbol
+write_cell(Symbol) :- write('|'), write(Symbol). % Symbol
 
