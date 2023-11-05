832eafbd7f891e3c610edb0886b73939adeefda5
xouteiro <xavierouteiro@gmail.com>
2023-11-05 23:30:15 +0000

    finished pc vs pc, not perfect

--- main.pl
+++ main.pl
@@ -14,7 +14,7 @@ play :-
         ;
         GameMode = 2 -> player_vs_computer
         ;
-        GameMode = 3 -> playing(player1)
+        GameMode = 3 -> computer_vs_computer
         ;
         write('Invalid game mode!'), nl, play
     ).
@@ -118,3 +118,47 @@ check_turn_change_with_computer(NewPlayer, PlayerTurn, Game) :-
         ;
         (Game = splut, game_over(PlayerTurn))
     ).
+
+
+    computer_vs_computer:-
+        HaveUsedLevitate is 0,
+        display_game(player1),
+        next_turn_ai_level1(player1, NewPlayer, Game, HaveUsedLevitate, UsedLevitate, StopLevitation),
+        display_game(player2),
+        HaveUsedLevitate1 is 0,
+        sleep_seconds(500),
+        next_turn_ai_level1(player2, NewPlayer1, Game1, HaveUsedLevitate1, UsedLevitate1, StopLevitation),
+        display_game(player2),
+        sleep_seconds(500),
+        check_used_levitate(HaveUsedLevitate1, UsedLevitate1, NewHaveUsedLevitate),
+        next_turn_ai_level1(player2, NewPlayer2, Game2, NewHaveUsedLevitate, UsedLevitate1, StopLevitation),  
+        computer_vs_computer_playing(player1).
+    
+    computer_vs_computer_playing(PlayerTurn):-
+        HaveUsedLevitate is 0,
+        display_game(PlayerTurn),
+        sleep_seconds(500),
+        check_used_levitate(HaveUsedLevitate, UsedLevitate, NewHaveUsedLevitate),
+        next_turn_ai_level1(PlayerTurn, NewPlayer, Game, NewHaveUsedLevitate, UsedLevitate, StopLevitation),
+        check_turn_change_with_cp_cp(NewPlayer, PlayerTurn, Game),
+        display_game(PlayerTurn),
+        sleep_seconds(500),
+        check_used_levitate(NewHaveUsedLevitate, UsedLevitate, NewHaveUsedLevitate1),
+        next_turn_ai_level1(PlayerTurn, NewPlayer1, Game1, NewHaveUsedLevitate1, UsedLevitate, StopLevitation),
+        check_turn_change_with_cp_cp(NewPlayer1, PlayerTurn, Game1),
+        display_game(PlayerTurn),
+        sleep_seconds(500),
+        check_used_levitate(NewHaveUsedLevitate1, UsedLevitate, NewHaveUsedLevitate2),
+        next_turn_ai_level1(PlayerTurn, NewPlayer2, Game2, NewHaveUsedLevitate2, UsedLevitate, StopLevitation),
+        check_turn_change_with_cp_cp(NewPlayer2, PlayerTurn, Game2),
+        ( PlayerTurn = player1 -> NextPlayer = player2 ; NextPlayer = player1 ),
+        computer_vs_computer_playing(NextPlayer).
+    
+    
+    check_turn_change_with_cp_cp(NewPlayer, PlayerTurn, Game) :-
+        (
+            not_inst(Game) -> (NewPlayer = PlayerTurn -> true ; computer_vs_computer_playing(NewPlayer))
+            ;
+            (Game = splut, game_over(PlayerTurn))
+        ).
+    
